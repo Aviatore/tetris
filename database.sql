e1b762cded0743d68b5540062b58d93d12389955
Wojciech Wesołowski <wojtek.wes@gmail.com>
2020-12-01 18:11:35 +0100

    Update the database.sql file

--- database.sql
+++ database.sql
@@ -1,4 +1,9 @@
-create table score (
+create table if not exists score (
     score_id serial primary key not null,
     score integer
 );
+
+-- The table 'score' needs at least one value for the database to work properly
+-- Below query adds a zero to the table
+insert into score(score)
+values(0);
